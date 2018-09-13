e9eb1fc4d964c90bd76a2001cbf74306b64c01bc
Joel Jensen <joel.jensen@experticity.com>
2018-09-13 13:48:32 -0600

    Split out some code to try to simplify NewHandler

--- autojson.go
+++ autojson.go
@@ -17,6 +17,76 @@ var (
 	type_http_req = reflect.TypeOf(http.Request{})
 )
 
+type in_idx struct {
+	ctx      int
+	req      int
+	http_req int
+	http_res int
+}
+
+type out_idx struct {
+	res  int
+	err  int
+	code int
+}
+
+func reflect_in(f reflect.Type) (in_idx, error) {
+	r := in_idx{
+		ctx:      -1,
+		req:      -1,
+		http_req: -1,
+		http_res: -1,
+	}
+	for i := 1; i < f.NumIn(); i++ {
+		in := f.In(i)
+
+		if r.ctx == -1 && in == type_context {
+			r.ctx = i
+			continue
+		}
+		if r.http_req == -1 && in == type_http_req {
+			r.http_req = i
+			continue
+		}
+		if r.http_res == -1 && in == type_http_res {
+			r.http_res = i
+			continue
+		}
+		// assume any leftover argment is a request
+		if r.req == -1 {
+			r.req = i
+			continue
+		}
+		return r, fmt.Errorf("Too many arguments: Not sure how to populate argument %d (%s)", i-1, in)
+	}
+	return r, nil
+}
+func reflect_out(f reflect.Type) (out_idx, error) {
+	r := out_idx{
+		res:  -1,
+		err:  -1,
+		code: -1,
+	}
+	for i := 0; i < f.NumOut(); i++ {
+		out := f.Out(i)
+
+		if r.err == -1 && out == type_error {
+			r.err = i
+			continue
+		}
+		if r.code == -1 && out == type_int {
+			r.code = i
+			continue
+		}
+		if r.res == -1 {
+			r.res = i
+			continue
+		}
+		return r, fmt.Errorf("Too many return values: Not sure what to do with value %d (%s)", i, out)
+	}
+	return r, nil
+}
+
 func NewHandler(service_i interface{}, method_name string) http.HandlerFunc {
 	service_v := reflect.ValueOf(service_i)
 	service_t := service_v.Type()
@@ -29,56 +99,13 @@ func NewHandler(service_i interface{}, method_name string) http.HandlerFunc {
 	method_fv := method.Func
 	method_ft := method.Type
 
-	in_ctx := -1
-	in_req := -1
-	in_http_req := -1
-	in_http_res := -1
-
-	out_res := -1
-	out_err := -1
-	out_code := -1
-
-	for i := 1; i < method_ft.NumIn(); i++ {
-		in := method_ft.In(i)
-
-		if in_ctx == -1 && in == type_context {
-			in_ctx = i
-			continue
-		}
-		if in_http_req == -1 && in == type_http_req {
-			in_http_req = i
-			continue
-		}
-		if in_http_res == -1 && in == type_http_res {
-			in_http_res = i
-			continue
-		}
-		// assume any leftover argment is a request
-		if in_req == -1 {
-			in_req = i
-			continue
-		}
-
-		panic(fmt.Errorf("NewHandler(%s, %#v) too many arguments: Not sure how to populate argument %d (%s)", service_t.String(), method_name, i-1, in))
+	in, err := reflect_in(method_ft)
+	if err != nil {
+		panic(fmt.Errorf("NewHandler(%s, %#v) %v", service_t.String(), method_name, err))
 	}
-
-	for i := 0; i < method_ft.NumOut(); i++ {
-		out := method_ft.Out(i)
-
-		if out_err == -1 && out == type_error {
-			out_err = i
-			continue
-		}
-		if out_code == -1 && out == type_int {
-			out_code = i
-			continue
-		}
-		if out_res == -1 {
-			out_res = i
-			continue
-		}
-
-		panic(fmt.Errorf("NewHandler(%s, %#v) too many return values: Not sure what to do with value %d (%s)", service_t.String(), method_name, i, out))
+	out, err := reflect_out(method_ft)
+	if err != nil {
+		panic(fmt.Errorf("NewHandler(%s, %#v) %v", service_t.String(), method_name, err))
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -87,19 +114,18 @@ func NewHandler(service_i interface{}, method_name string) http.HandlerFunc {
 		args := make([]reflect.Value, method_ft.NumIn())
 		args[0] = service_v
 
-		if in_ctx != -1 {
-			args[in_ctx] = reflect.ValueOf(r.Context())
+		if in.ctx != -1 {
+			args[in.ctx] = reflect.ValueOf(r.Context())
 		}
-		if in_http_req != -1 {
-			args[in_http_req] = reflect.ValueOf(r)
+		if in.http_req != -1 {
+			args[in.http_req] = reflect.ValueOf(r)
 		}
-		if in_http_res != -1 {
-			args[in_http_res] = reflect.ValueOf(w)
+		if in.http_res != -1 {
+			args[in.http_res] = reflect.ValueOf(w)
 		}
-		if in_req != -1 {
-			in := method_ft.In(in_req)
-
-			in_v := reflect.New(in)
+		if in.req != -1 {
+			in_f := method_ft.In(in.req)
+			in_v := reflect.New(in_f)
 			in_i := in_v.Interface()
 
 			d := json.NewDecoder(r.Body)
@@ -111,10 +137,10 @@ func NewHandler(service_i interface{}, method_name string) http.HandlerFunc {
 				return
 			}
 
-			if in.Kind() == reflect.Ptr {
-				args[in_req] = in_v
+			if in_f.Kind() == reflect.Ptr {
+				args[in.req] = in_v
 			} else {
-				args[in_req] = in_v.Elem()
+				args[in.req] = in_v.Elem()
 			}
 		}
 
@@ -126,14 +152,14 @@ func NewHandler(service_i interface{}, method_name string) http.HandlerFunc {
 			v_out_res  interface{}
 		)
 
-		if out_code != -1 {
-			v_out_code, _ = res_vals[out_code].Interface().(int)
+		if out.code != -1 {
+			v_out_code, _ = res_vals[out.code].Interface().(int)
 		}
-		if out_err != -1 {
-			v_out_err, _ = res_vals[out_err].Interface().(error)
+		if out.err != -1 {
+			v_out_err, _ = res_vals[out.err].Interface().(error)
 		}
-		if out_res != -1 {
-			v_out_res = res_vals[out_res].Interface()
+		if out.res != -1 {
+			v_out_res = res_vals[out.res].Interface()
 		}
 
 		// if you set http return code -1, that means
@@ -147,7 +173,7 @@ func NewHandler(service_i interface{}, method_name string) http.HandlerFunc {
 		// if you don't specify a http code, default to 500 or 200
 		if v_out_code == 0 {
 			if v_out_err == nil {
-				if out_res == -1 {
+				if out.res == -1 {
 					v_out_code = 204
 				} else {
 					v_out_code = 200
@@ -165,7 +191,7 @@ func NewHandler(service_i interface{}, method_name string) http.HandlerFunc {
 		// if you don't have a return value at all, return an empty response body.
 		// (as opposed to "null", if you return nil).  in this case, don't return
 		// a content type.
-		if out_res == -1 {
+		if out.res == -1 {
 			w.WriteHeader(v_out_code)
 			return
 		}
